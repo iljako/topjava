4ad399f02fdd141f7daa628015519b1ab0506aac
iljako <iljakokorev@gmail.com>
2026-06-13 20:46:02 +0300

    HW5 - Fix after review - Удалены неиспользуемые ROW_MAPPER - Добавлены тесты getWithUser и getWithMeals для DataJpa*

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqlJdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqlJdbcMealRepository.java
@@ -2,35 +2,16 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.Profiles;
-import ru.javawebinar.topjava.model.Meal;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Profile(Profiles.HSQL_DB)
 @Repository
 public class HsqlJdbcMealRepository extends AbstractJdbcMealRepository<Timestamp> {
-
-    private static final RowMapper<Meal> ROW_MAPPER = new RowMapper<Meal>() {
-        @Override
-        public Meal mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Meal meal = new Meal();
-            meal.setId(rs.getInt("id"));
-            meal.setDescription(rs.getString("description"));
-            meal.setCalories(rs.getInt("calories"));
-            Timestamp timestamp = rs.getTimestamp("date_time");
-            if (timestamp != null) {
-                meal.setDateTime(timestamp.toLocalDateTime());
-            }
-            return meal;
-        }
-    };
 
     public HsqlJdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         super(jdbcTemplate, namedParameterJdbcTemplate);
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,10 +1,22 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_MATCHER;
 
 @ActiveProfiles(profiles = Profiles.DATAJPA)
 public class DataJpaMealServiceTest extends AbstractMealServiceTest {
-
+    @Test
+    public void getWithUser() {
+        Meal meal = service.getWithUser(MEAL1_ID, USER_ID);
+        MEAL_MATCHER.assertMatch(meal, meal1);
+        USER_MATCHER.assertMatch(meal.getUser(), UserTestData.user);
+    }
 }
